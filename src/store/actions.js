cb4c362921a1b27a890cd52a19761535a179ad45
João Fernandes <joaovitorfernandes2@gmail.com>
2018-02-24 14:34:05 -0300

    Add notes actions and mutations
    Also set notes intial state to an array

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,1 +1,13 @@
-export default {};
+export default {
+  addNote(context, note) {
+    context.commit("ADD_NOTE", note);
+  },
+
+  removeNote(context, index) {
+    context.commit("REMOVE_NOTE", index);
+  },
+
+  editNote(context, { index, note }) {
+    context.commit("EDIT_NOTE", { index, note });
+  }
+};
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,1 +1,13 @@
-export default {};
+export default {
+  ADD_NOTE(state, note) {
+    state.notes.push(note);
+  },
+
+  REMOVE_NOTE(state, index) {
+    state.notes.splice(index, 1);
+  },
+
+  EDIT_NOTE(state, { index, note }) {
+    state.notes[index] = note;
+  }
+};
