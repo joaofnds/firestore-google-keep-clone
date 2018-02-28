872c86829662425d4827b71411cf9657afd47e4b
João Fernandes <joaovitorfernandes2@gmail.com>
2018-02-28 02:37:10 -0300

    Add firebase dependency and initial config

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyD3DB506Rnx7186JRJNN7e3sO21isyEWWU",
+  authDomain: "project--1859386222849825921.firebaseapp.com",
+  databaseURL: "https://project--1859386222849825921.firebaseio.com",
+  projectId: "project--1859386222849825921",
+  storageBucket: "project--1859386222849825921.appspot.com",
+  messagingSenderId: "256405949063"
+};
