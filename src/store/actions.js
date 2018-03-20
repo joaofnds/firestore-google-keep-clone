0765187861dedf72ee39fd050935109bbd7c2712
João Fernandes <joaofnds@joaofnds-macbook-pro.local>
2018-03-20 07:41:02 -0300

    Check for user before trying to set data to Firestore

--- src/store/actions.js
+++ src/store/actions.js
@@ -10,33 +10,38 @@ export function setNotes(context, notes) {
 }
 
 export function setNote(context, { noteID, note }) {
-  const userID = context.state.user.uid;
-
   // Commit changes to state
   context.commit("ADD_NOTE", { noteID, note });
 
   // Set note to firestore
-  setFirebaseNote(userID, noteID, note)
-    .then(() => showNotification("Note saved"))
-    .catch(err => {
-      showNotification("Failed to save note on Firestore");
-      throw new Error("Failed to save note to firebase", err);
-    });
+  const { user } = context.state;
+  if (user) {
+    const userID = user.uid;
+    setFirebaseNote(userID, noteID, note)
+      .then(() => showNotification("Note saved"))
+      .catch(err => {
+        showNotification("Failed to save note on Firestore");
+        throw new Error("Failed to save note to firebase", err);
+      });
+  }
 }
 
 export function deleteNote(context, noteID) {
-  const userID = context.state.user.uid;
   const note = context.state.notes[noteID];
 
   // Commit changes to state
   context.commit("DELETE_NOTE", noteID);
 
   // Delete note from firestore
-  deleteFirebaseNote(userID, noteID)
-    .then()
-    .catch(err => {
-      throw new Error("Failed to delete note", err);
-    });
+  const { user } = context.state;
+  if (user) {
+    const userID = user.uid;
+    deleteFirebaseNote(userID, noteID)
+      .then()
+      .catch(err => {
+        throw new Error("Failed to delete note", err);
+      });
+  }
 
   // Display notification
   showNotification("Note deleted", 2000, "UNDO", () => {
