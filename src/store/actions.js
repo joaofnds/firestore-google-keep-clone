57355eea733f59b0c21e87e7cfdfef1fee21d7ff
João Fernandes <joaofnds@joaofnds-macbook-pro.local>
2018-03-20 07:32:30 -0300

    Split actions export. Commit changes to the state immediately
    Also add notifications with the possibility to undo deletions

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,38 +3,59 @@ import {
   deleteNote as deleteFirebaseNote
 } from "../firebase/notes";
 
+import showNotification from "../common/showNotification";
+
+export function setNotes(context, notes) {
+  context.commit("SET_NOTES", notes);
+}
+
+export function setNote(context, { noteID, note }) {
+  const userID = context.state.user.uid;
+
+  // Commit changes to state
+  context.commit("ADD_NOTE", { noteID, note });
+
+  // Set note to firestore
+  setFirebaseNote(userID, noteID, note)
+    .then(() => showNotification("Note saved"))
+    .catch(err => {
+      showNotification("Failed to save note on Firestore");
+      throw new Error("Failed to save note to firebase", err);
+    });
+}
+
+export function deleteNote(context, noteID) {
+  const userID = context.state.user.uid;
+  const note = context.state.notes[noteID];
+
+  // Commit changes to state
+  context.commit("DELETE_NOTE", noteID);
+
+  // Delete note from firestore
+  deleteFirebaseNote(userID, noteID)
+    .then()
+    .catch(err => {
+      throw new Error("Failed to delete note", err);
+    });
+
+  // Display notification
+  showNotification("Note deleted", 2000, "UNDO", () => {
+    setNote(context, { noteID, note });
+  });
+}
+
+export function setUser(context, user) {
+  context.commit("SET_USER", user);
+}
+
+export function unsetUser(context) {
+  context.commit("UNSET_USER");
+}
+
 export default {
-  setNotes(context, notes) {
-    context.commit("SET_NOTES", notes);
-  },
-
-  setNote(context, { noteID, note }) {
-    const userID = context.state.user.uid;
-    setFirebaseNote(userID, noteID, note)
-      .then(() => {
-        context.commit("ADD_NOTE", { noteID, note });
-      })
-      .catch(err => {
-        throw new Error("Failed to save note to firebase", err);
-      });
-  },
-
-  deleteNote(context, noteID) {
-    const userID = context.state.user.uid;
-    deleteFirebaseNote(userID, noteID)
-      .then(() => {
-        context.commit("DELETE_NOTE", noteID);
-      })
-      .catch(err => {
-        throw new Error("Failed to delete note", err);
-      });
-  },
-
-  setUser(context, user) {
-    context.commit("SET_USER", user);
-  },
-
-  unsetUser(context) {
-    context.commit("UNSET_USER");
-  }
+  setNotes,
+  setNote,
+  deleteNote,
+  setUser,
+  unsetUser
 };
