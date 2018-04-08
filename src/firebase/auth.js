342033ef4863441d28d4b9575a6dd36c8b85255c
João Fernandes <joaovitorfernandes2@gmail.com>
2018-04-08 15:50:57 -0300

    Set firebase auth persistance

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,17 +1,28 @@
 import { auth } from "firebase";
 
+const firebaseAuth = auth();
 const signInProvider = new auth.GoogleAuthProvider();
 
 /**
  * Signs in the user using Google Auth Provider
  */
 export function signIn() {
-  return auth().signInWithPopup(signInProvider);
+  return new Promise((resolve, reject) => {
+    firebaseAuth
+      .setPersistence(auth.Auth.Persistence.LOCAL)
+      .then(() => {
+        return firebaseAuth
+          .signInWithPopup(signInProvider)
+          .then(resolve)
+          .catch(reject);
+      })
+      .catch(reject);
+  });
 }
 
 /**
  * Signs out the current logged in user
  */
 export function signOut() {
-  return auth().signOut();
+  return firebaseAuth.signOut();
 }
