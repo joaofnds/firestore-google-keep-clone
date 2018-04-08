9b2c2a72f466c16401ef0568bf2cec1d411523ab
João Fernandes <joaovitorfernandes2@gmail.com>
2018-04-08 15:50:03 -0300

    Change vuex store actions to be exported as one object

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,76 +1,63 @@
 import {
-  setNote as setFirebaseNote,
-  deleteNote as deleteFirebaseNote
+  setNote as setFirestoreNote,
+  deleteNote as deleteFirestoreNote
 } from "../firebase/notes";
-
 import showNotification from "../common/showNotification";
 
-export function setNotes(context, notes) {
-  context.commit("SET_NOTES", notes);
-}
-
-export function mergeNotes(context, notes) {
-  const { user, notes: stateNotes } = context.state;
-  if (user) {
-    for (let noteID in stateNotes) {
-      setFirebaseNote(user.uid, noteID, stateNotes[noteID]);
-    }
-  }
-  setNotes(context, { ...stateNotes, ...notes });
-}
-
-export function setNote(context, { noteID, note }) {
-  // Commit changes to state
-  context.commit("ADD_NOTE", { noteID, note });
-
-  // Set note to firestore
-  const { user } = context.state;
-  if (user) {
-    setFirebaseNote(user.uid, noteID, note)
-      .then(() => showNotification("Note saved"))
-      .catch(err => {
-        showNotification("Failed to save note to Firestore");
-        throw new Error("Failed to save note to firebase", err);
-      });
-  }
-}
-
-export function deleteNote(context, noteID) {
-  const { notes: { [noteID]: note } } = context.state;
-
-  // Commit changes to state
-  context.commit("DELETE_NOTE", noteID);
-
-  // Delete note from firestore
-  const { user } = context.state;
-  if (user) {
-    deleteFirebaseNote(user.uid, noteID)
-      .then()
-      .catch(err => {
-        showNotification("Failed to delete note from Firestore");
-        throw new Error("Failed to delete note from Firestore", err);
-      });
-  }
-
-  // Display notification
-  showNotification("Note deleted", 2000, "UNDO", () => {
-    setNote(context, { noteID, note });
-  });
-}
-
-export function setUser(context, user) {
-  context.commit("SET_USER", user);
-}
-
-export function unsetUser(context) {
-  context.commit("UNSET_USER");
-}
-
 export default {
-  setNotes,
-  mergeNotes,
-  setNote,
-  deleteNote,
-  setUser,
-  unsetUser
+  setNotes(context, notes) {
+    context.commit("SET_NOTES", notes);
+  },
+
+  mergeNotes(context, notes) {
+    const { notes: stateNotes } = context.state;
+    context.dispatch("setNotes", { ...stateNotes, ...notes });
+  },
+
+  setNote(context, { noteID, note }) {
+    // Commit changes to state
+    context.commit("ADD_NOTE", { noteID, note });
+
+    // Set note to firestore
+    const { user } = context.state;
+    if (user) {
+      setFirestoreNote(user.uid, noteID, note)
+        .then(() => showNotification("Note saved"))
+        .catch(err => {
+          showNotification("Failed to save note to Firestore");
+          throw new Error("Failed to save note to Firestore", err);
+        });
+    }
+  },
+
+  deleteNote(context, noteID) {
+    const { notes: { [noteID]: note } } = context.state;
+
+    // Commit changes to state
+    context.commit("DELETE_NOTE", noteID);
+
+    // Delete note from firestore
+    const { user } = context.state;
+    if (user) {
+      deleteFirestoreNote(user.uid, noteID)
+        .then()
+        .catch(err => {
+          showNotification("Failed to delete note from Firestore");
+          throw new Error("Failed to delete note from Firestore", err);
+        });
+    }
+
+    // Display notification
+    showNotification("Note deleted", 2000, "UNDO", () => {
+      context.dispatch("setNote", { noteID, note });
+    });
+  },
+
+  setUser(context, user) {
+    context.commit("SET_USER", user);
+  },
+
+  unsetUser(context) {
+    context.commit("UNSET_USER");
+  }
 };
